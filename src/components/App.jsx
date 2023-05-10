2fd6fec44f7277fdc0f0a19cc33e0ffd50f1513e
KRKDEV <111200477+KRKDEV@users.noreply.github.com>
2023-05-10 21:00:10 +0200

    Aded pages components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,16 @@
+import { Routes, Route } from 'react-router-dom';
 import Navigation from './Navigation/Navigation';
+import Home from '../pages/Home/Home';
+import Movies from '../pages/Movies/Movies';
 
 export const App = () => {
   return (
     <div className="app">
       <Navigation />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Movies />} />
+      </Routes>
     </div>
   );
 };
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,15 +1,25 @@
-// import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import css from './Navigation.module.css';
 
 const Navigation = () => {
   //   const navigate = useNavigate();
+  const location = useLocation();
   return (
-    <nav className={css.navigation}>
-      <a href="/">Home </a>
-      <a href="/">Movies</a>
-
-      {/* <Link to="/">Home</Link>
-      <Link to="/">Movies</Link> */}
+    <nav className={css.nav}>
+      <Link
+        to="/"
+        className={location.pathname === '/' ? css.active : css['nav__link']}
+      >
+        Home
+      </Link>
+      <Link
+        to="/movies"
+        className={
+          location.pathname === '/movies' ? css.active : css['nav__link']
+        }
+      >
+        Movies
+      </Link>
     </nav>
   );
 };
